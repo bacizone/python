9ed5b48acff3351900cf273799b02a69cdf8d542
BaciZone <bacizone@gmail.com>
2019-01-03 07:55:49 +0100

    Ch6 excercises added

---
+++ ch6-bark.py
@@ -0,0 +1,20 @@
+print('Get those dogs ready')
+
+def bark(name, weight):
+    if weight > 20:
+        print(name, 'says WOOF, WOOF')
+    else:
+        print(name, 'says woof, woof')
+
+bark('Codie', 40)
+bark('Sparky', 9)
+bark('Jackon', 12)
+bark('Fido', 65)
+bark('Speedy', 20)
+bark('Barnaby', -1)
+# bark('Scoottie', 0, 0)
+# bark('Lady', '20')
+bark('Spot', 10)
+bark('Rover', 21)
+
+print("'Okay, we're all done")
