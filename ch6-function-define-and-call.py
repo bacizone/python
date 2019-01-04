9c0156dd7cbf3aadb18e1fb513efdf2c3d2fcbf6
BaciZone <bacizone@gmail.com>
2019-01-04 07:56:50 +0100

    adding more examples

--- ch6-function-define-and-call.py
+++ ch6-function-define-and-call.py
@@ -6,3 +6,39 @@ def get_bark(weight):
 
 codies_bark = get_bark(40)
 print("Codie's bark", codies_bark)
+
+# NEW EXAMPLE
+def allow_access(person):
+    if person == 'Dr Evil':
+        answer = True
+    else:
+        answer = False
+    return answer
+
+# NEW EXAMPLE
+def compute(x,y):
+    total = x + y
+    if (total > 10):
+        total = 10
+    return total
+
+result = compute(2,3)
+result = compute(11,3)
+
+print(result)
+
+# NEW EXAMPLE
+hair = input('What color hair [brown]?')
+if hair == '':
+    hair = 'brown'
+print('You choose', hair)
+
+# MY PSEUDOCODE
+
+hair = input ('What color hair')
+hair_length = input ('What hair length')
+
+person (hair, hair_length, eyes, gender, has_glasses, has beard):
+name 'has' hair, hair_length, eyes, gender, has no glasses and has beard.
+
+person_Katie ('blonde', 'long', 'blue', 'woman', 'no', 'no')
