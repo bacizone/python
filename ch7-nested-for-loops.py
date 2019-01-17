1869d1d0e77da6e3182eb09e8bf6848feaed2f23
BaciZone <bacizone@gmail.com>
2019-01-17 07:41:31 +0100

    bubble sorting + loop

--- ch7-nested-for-loops.py
+++ ch7-nested-for-loops.py
@@ -1,6 +1,27 @@
-#use debugger steps to analyze control flow
-for i in range(0,4):
-    for j in range (0,4):
-        result = (i * j)
+# #use debugger steps to analyze control flow
+# for i in range(0,4):
+#     for j in range (0,4):
+#         result = (i * j)
 
-print(result)
+# print(result)
+
+# for word in ('ox', 'cat', 'lion', 'tiger', 'bobcat'):
+#         for i in range(2, 7):
+#                 letters = len(word)
+#                 if (letters % i) == 0:
+#                         print(i, word)
+
+full = False
+donations = []
+full_load = 45
+
+toys = ['robot', 'doll', 'ball', 'slinky']
+
+while not full:
+        for toy in toys:
+                donations.append(toy)
+                size = len(donations)
+                if (size >= full_load):
+                        full = True
+print('Full with', len(donations), 'toys')
+print(donations)
