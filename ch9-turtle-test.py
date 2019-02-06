940125fbe8981e78d8dc5e79f527fa4a54fd55bd
BaciZone <bacizone@gmail.com>
2019-02-06 17:50:00 +0100

    turtle code added

--- ch9-turtle-test.py
+++ ch9-turtle-test.py
@@ -2,14 +2,56 @@ import turtle
 
 slowpoke = turtle.Turtle()
 slowpoke.shape('turtle')
+slowpoke.color('blue')
+pokey = turtle.Turtle()
+pokey.shape('turtle')
+pokey.color('red')
 
-slowpoke.forward(100)
-slowpoke.right(90)
-slowpoke.forward(100)
-slowpoke.right(90)
-slowpoke.forward(100)
-slowpoke.right(90)
-slowpoke.forward(100)
-slowpoke.right(90)
+slowpoke.pencolor('blue')
+slowpoke.penup()
+slowpoke.setposition(-120, 0)
+slowpoke.pendown()
+slowpoke.circle(50)
+
+slowpoke.pencolor('red')
+slowpoke.penup()
+slowpoke.setposition(120, 0)
+slowpoke.pendown()
+slowpoke.circle(50)
+
+# def make_square(the_turtle):
+#     for i in range(0,4):
+#         the_turtle.forward(100)
+#         the_turtle.right(90)
+
+def make_spiral(the_turtle):
+    # for i in range(0, 36):
+    #     make_square(the_turtle)
+    #     the_turtle.right(10)
+
+    for i in range(5):
+        slowpoke.forward(100)
+        slowpoke.right(144)
+
+def make_shape(t, sides):
+    angle = 360/sides
+    for i in range(0, sides):
+        t.forward(100)
+        t.right(angle)
+
+make_shape(slowpoke, 3)
+make_shape(slowpoke, 5)
+make_shape(slowpoke, 8)
+make_shape(slowpoke, 10)
+
+
+
+# make_square(slowpoke)
+# pokey.right(45)
+# make_square(pokey)
+
+make_spiral(slowpoke)
+pokey.right(5)
+make_spiral(pokey)
 
 turtle.mainloop()
